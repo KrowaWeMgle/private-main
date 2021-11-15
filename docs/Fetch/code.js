90a8c6a8a7e2646b2831aec58746cfe150fd41fe
KrowaWeMgle <92986633+KrowaWeMgle@users.noreply.github.com>
2021-11-15 14:27:42 +0100

    Update code.js

--- docs/Fetch/code.js
+++ docs/Fetch/code.js
@@ -15,7 +15,7 @@ verseChose.onchange = function() {
 function updateDisplay(verse) {
   verse = verse.replace(" ", "");
   verse.toLowerCase();
-  let url = `docs/Fetch/textExample/${verse}.txt`;
+  let url = `Fetch/textExample/${verse}.txt`;
   let request = new XMLHttpRequest();
   request.open('GET', url);
   request.responseType = 'text';
